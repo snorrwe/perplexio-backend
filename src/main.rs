4c461153a7b45e7421d6bb22c2b398e6221d91d0
Kiss Dániel <tv7bks@inf.elte.hu>
2018-12-28 13:02:36 +0100

    Add put and delete to CORS policy

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() {
 
     let options = rocket_cors::Cors {
         allowed_origins: allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post]
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete]
             .into_iter()
             .map(From::from)
             .collect(),
